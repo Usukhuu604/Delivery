23c502f6ac6dd0d2c2edf57de3f119cab37d5481
Usukhuu64 <24lp0723@nest.edu.mn>
2025-05-27 03:29:47 +0800

    adonei1

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { connectDatabase } from "./database";
 import { configDotenv } from "dotenv";
-import { authRouter, foodCategoryRouter } from "./routers";
+import { authRouter, foodCategoryRouter, foodRouter, foodOrderRouter } from "./routers";
 
 const app = express();
 
@@ -12,7 +12,10 @@ const port = 8000;
 
 app.use(express.json());
 
+
 app.use("/auth", authRouter);
 app.use("/food-category", foodCategoryRouter);
+app.use("/food", foodRouter);
+app.use("/food-order", foodOrderRouter);
 
 app.listen(port, () => console.log(`http://localhost:${port}`));
