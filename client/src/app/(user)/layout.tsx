b3a649fda49648d76e967a56ad4bdf0c33922bde
Usukhuu64 <24lp0723@nest.edu.mn>
2025-05-29 01:16:47 +0800

    latest32

---
+++ client/src/app/(user)/layout.tsx
@@ -0,0 +1,8 @@
+import { UserContextProvider } from "@/providers/UserProvider";
+
+const Layout = () => {
+  <UserContextProvider>
+    <div>laytout</div>
+  </UserContextProvider>;
+};
+export default Layout;
---
+++ client/src/providers/UserProvider.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { createContext, PropsWithChildren, useState } from "react";
+
+type User = {
+  email: string;
+  password: string;
+};
+
+type UserContextType = {
+  user: User;
+};
+
+const UserContext = createContext<UserContextType>({} as UserContextType);
+
+export const UserContextProvider = ({ children }: PropsWithChildren) => {
+  const [user, setUser] = useState<User>({ email: "", password: "" });
+
+  return <UserContext.Provider value={{ user }}>{children}</UserContext.Provider>;
+};
